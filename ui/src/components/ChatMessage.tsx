8b53a9fdfe3c2512eb76f1a95e77b94f24b4be59
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2023-11-09 21:23:06 -0500

    implement copy button for code

--- ui/src/components/ChatMessage.tsx
+++ ui/src/components/ChatMessage.tsx
@@ -1,4 +1,4 @@
-import { type Component, createSignal, Accessor } from "solid-js";
+import { type Component, createSignal, Accessor, JSX, For } from "solid-js";
 import "./ChatMessage.css";
 import DOMPurify from "dompurify";
 import { Marked } from "marked";
@@ -8,6 +8,7 @@ import hljs from "highlight.js";
 import syntaxThemeLight from "highlight.js/styles/atom-one-light.min.css?url";
 import syntaxThemeDark from "highlight.js/styles/atom-one-dark.min.css?url";
 import katexStyles from "../assets/katex.min.css?url";
+import { template } from "solid-js/web";
 
 // Configure marked to highlight code
 const marked = new Marked(
@@ -31,7 +32,20 @@ export const ChatMessage: Component<{ data: ChatMessageData }> = (props) => {
       .message()
       // Remove zero-width characters that mess with marked
       .replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, "");
-    return DOMPurify.sanitize(marked.parse(message) as string);
+    const src = DOMPurify.sanitize(marked.parse(message) as string);
+    let elem = template(`<div>${src}</div>`) as unknown as HTMLDivElement;
+    elem = elem.cloneNode(true) as HTMLDivElement;
+    for (const code of elem.querySelectorAll(".hljs")) {
+      const codeText = code.textContent || "";
+      const button = document.createElement("button");
+      button.classList.add("copy-button");
+      button.addEventListener("click", () => {
+        navigator.clipboard.writeText(codeText);
+        setTimeout(() => button.blur(), 2000);
+      });
+      code.appendChild(button);
+    }
+    return Array.from(elem.children);
   };
   return (
     <div
@@ -40,8 +54,9 @@ export const ChatMessage: Component<{ data: ChatMessageData }> = (props) => {
         ai: role == "ai",
         user: role == "user",
       }}
-      innerHTML={markdown()}
-    ></div>
+    >
+      {markdown()}
+    </div>
   );
 };
 
